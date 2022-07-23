2cd02d041513295c9cd288276e13a4413a4cb05d
Yasmin <38674384+YasminBarbosa@users.noreply.github.com>
2022-07-23 23:02:51 +0000

    Incrementando código com decisões encadeadas

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+# nome = (input("Digite o nome: "))
+# idade = int(input("Digite a idade: "))
+# if idade>=65:
+#   print("O paciente " + nome + " POSSUI atendimento prioritário! ")
+# else:
+#   print("O paciente " + nome + " NÃO possui atendimento prioritário")
+# print("FIM")
+
+
 nome = (input("Digite o nome: "))
 idade = int(input("Digite a idade: "))
-if idade>=65:
-  print("O paciente " + nome + " POSSUI atendimento prioritário! ")
+doenca_infectocontagiosa = (
+    input("Suspeita de doença infectocontagiosa? ")).upper()
+if idade >= 65:
+    print("O paciente " + nome + " POSSUI atendimento prioritário! ")
+elif doenca_infectocontagiosa == "SIM":
+    print("O paciente " + nome +
+          " dave ser atendido na sala de espera reservada.")
 else:
-  print("O paciente " + nome + " NÃO possui atendimento prioritário")
+    print("O paciente " + nome + " NÃO possui atendimento prioritário")
 print("FIM")
